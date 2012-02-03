5c40281f86d687088e423b1e49be57d251d6ed7d
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2012-02-03 09:59:24 -0200

    first version

---
+++ src/MissingKeys.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class MissingKeys {
+	public static void main(String[] args) throws IOException {
+		Properties en = new Properties();
+		en.load(new FileInputStream("src/main/resources/messages.properties"));
+
+		Properties base = new Properties();
+		base.load(new FileInputStream("src/main/resources/messages_pt_BR.properties"));
+		
+		TreeSet<String> missing = new TreeSet<String>();
+		for(Object key : base.keySet()) {
+			if(en.getProperty((String) key)==null) {
+				missing.add((String) key);
+			}
+		}
+		for(String key : missing) {
+			System.out.println(key + " = " + base.getProperty(key));
+		}
+	}
+}
